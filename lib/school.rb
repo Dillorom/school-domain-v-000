764672e8a156bbaca019dbed41f61b4007ad5422
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-05-04 19:41:52 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,11 +11,7 @@ attr_accessor :name, :roster
     @roster = roster
   end
 
-  def roster
-    @roster
-  end
-
-def add_student(name, grade)
+  def add_student(name, grade)
     roster[grade] = []
     roster[grade] << name
   end
